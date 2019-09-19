aa6bd5058b8a64255ed50a15ccf914ca770605c1
BrianVanBogaert <s100952@ap.be>
2019-09-19 08:54:31 +0200

    Leerkracht toegevoegd

---
+++ team11/Leerkracht.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace team11
+{
+    class Leerkracht
+    {
+        public int Leeftijd { get; set; }
+        public string Naam { get; set; }
+    }
+}
